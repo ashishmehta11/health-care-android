3081863dfb7ca6a2680de816d34272a0cb1dce12
ashishmehta11 <ashish11mehta11@gmail.com>
2021-06-27 09:42:14 +0530

    Initial commit

---
+++ app/src/main/java/com/project/healthcare/home/Home.java
@@ -0,0 +1,16 @@
+package com.project.healthcare.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.project.healthcare.R;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
